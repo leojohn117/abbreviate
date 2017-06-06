1f1ff10485234d213a92a5cf165d7b42d521a147
leojohn117 <leojohn7@gmail.com>
2017-06-06 09:15:59 +0100

    truthy check added

--- abbreviate.js
+++ abbreviate.js
@@ -6,7 +6,7 @@
 'use strict';
 
 module.exports = function abbreviate(string) {
-	return string.replace(/\w{4,}/g, function (word) {
-    	return word[0] + (word.length - 2) + word.slice(-1);
-  	});
+	return string ? string.replace(/\w{4,}/g, function (word) {
+	return word[0] + (word.length - 2) + word.slice(-1);
+	}) : '' ;
 }
