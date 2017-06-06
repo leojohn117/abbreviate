d4ee1e8998da57d0fcb24dd2b97d4e392e271ad7
leojohn117 <leojohn7@gmail.com>
2017-06-06 09:16:46 +0100

    Update abbreviate.js

--- abbreviate.js
+++ abbreviate.js
@@ -7,6 +7,6 @@
 
 module.exports = function abbreviate(string) {
 	return string ? string.replace(/\w{4,}/g, function (word) {
-	return word[0] + (word.length - 2) + word.slice(-1);
+		return word[0] + (word.length - 2) + word.slice(-1);
 	}) : '' ;
 }
